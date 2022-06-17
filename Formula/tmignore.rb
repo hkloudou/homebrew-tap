5d2b88f314b8959741081a9e93de9a19d9e9356a
hkloudou <36762546+hkloudou@users.noreply.github.com>
2022-06-17 19:43:23 +0800

    Release `tmignore` v1.2.8

--- Formula/tmignore.rb
+++ Formula/tmignore.rb
@@ -1,8 +1,8 @@
 class Tmignore < Formula
 	desc "Exclude development files from Time Machine backups"
 	homepage "https://github.com/hkloudou/tmignore"
-	url "https://github.com/hkloudou/tmignore/archive/v1.2.2.tar.gz"
-	sha256 "37cca0593f9c3919b49cbb4f76d0242280c9eb771aaeecc72025d81acb409e30"
+	url "https://github.com/hkloudou/tmignore/archive/v1.2.8.tar.gz"
+	sha256 "c67b801ce8dddf01a2765a2586f7e5ef5935f28ce42d5e990447320887e79f4f"
 	head "https://github.com/hkloudou/tmignore.git"
 
 	depends_on :macos => :high_sierra
