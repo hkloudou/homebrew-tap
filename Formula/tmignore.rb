dd86d05ecaaa7729e06cd80f89544c9267761602
sample <sample@example.com>
2022-06-17 20:28:45 +0800

    13

--- Formula/tmignore.rb
+++ Formula/tmignore.rb
@@ -1,7 +1,7 @@
 class Tmignore < Formula
 	desc "Exclude development files from Time Machine backups"
 	homepage "https://github.com/hkloudou/tmignore"
-	url "https://github.com/hkloudou/tmignore/archive/v1.2.9.tar.gz"
+	url "https://github.com/hkloudou/tmignore/archive/v1.2.13.tar.gz"
 	sha256 "4bf99e90ccff57eb94a87cb10d91b0e5c9207618cf2f627ed9290ad6f5670e5a"
 	head "https://github.com/hkloudou/tmignore.git"
 
